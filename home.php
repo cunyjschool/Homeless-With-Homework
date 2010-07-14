70ddac890258e3f31dcaa1515c09e2c307314872
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-07-14 08:43:48 -0700

    Moved homepage content to home.php

---
+++ home.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+	<!-- Middle Starts -->
+	<div id="middle-out-top">
+	<div id="middle-out-bottom">
+	<div id="middle-content" <?php if ( comments_open() ) : ?>class="single"<?php endif; ?>>
+	<div id="middle-content-bottom">
+		<!-- Content Starts -->
+		<div id="content" class="wrap">
+			<div class="col-left">
+				<div id="main-content">
+				
+				<!-- Sing Post Starts -->
+					<div class="page post wrap">
+
+						<p style="margin: 0px;"><a href="<?php bloginfo('url'); ?>/?page_id=712"><img class="aligncenter size-full wp-image-737" style="margin: 0px;" title="top-tile2" src="<?php bloginfo('url'); ?>/wp-content/uploads/2010/05/top-tile21.jpg" alt="" width="810" height="80" /></a></p>
+
+						<table border="0">
+						<tbody>
+						<tr>
+						<td><a style="text-decoration: none;" href="<?php bloginfo('url'); ?>/?cat=3"><img class="size-full wp-image-468 alignnone" title="In Their Own Words" src="<?php bloginfo('url'); ?>/wp-content/uploads/2010/05/carandpeople-newwwtext.jpg" alt="" width="405" height="290" /></a></td>
+						<td><a style="text-decoration: none;" href="<?php bloginfo('url'); ?>/?cat=10"><img class="size-full wp-image-466 alignnone" title="The Right to an Education" src="<?php bloginfo('url'); ?>/wp-content/uploads/2010/05/Pringle-newwwtext.jpg" alt="" width="405" height="290" /></a></td>
+						</tr>
+						<tr>
+						<td><a href="<?php bloginfo('url'); ?>/?cat=9"><img class="size-full wp-image-453 alignnone" title="A Roof Over Their Heads" src="<?php bloginfo('url'); ?>/wp-content/uploads/2010/05/pregnantwoman-new-text.jpg" alt="" width="405" height="290" /></a></td>
+						<td><a style="text-decoration: none;" href="<?php bloginfo('url'); ?>/?cat=8"><img class="size-full wp-image-464 alignnone" title="By the Numbers" src="<?php bloginfo('url'); ?>/wp-content/uploads/2010/05/mapzoooooom-newtext.jpg" alt="" width="405" height="289" /></a></td>
+						</tr>
+						</tbody>
+						</table>
+
+					</div>
+						<!-- Sing Post Ends -->
+						
+				</div>
+				
+			</div>
+			
+			<div class="col-right">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+		<!-- Content Ends -->
+	</div>
+	</div>
+	</div>
+	</div>
+	<!-- Middle Ends -->
+	<?php get_footer(); ?>
